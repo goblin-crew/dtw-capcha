4cc54c52e0d6aee4152d482b97fce39eb8d08e5c
EloiMusk <elioteuber@outlook.com>
2023-06-21 07:30:30 +0200

    feat: adding tensorflow ocr training scripts

---
+++ captcha1/sample_collector.py
@@ -0,0 +1,80 @@
+import random
+import re
+import time
+
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+
+# Function to sanitize the filename
+def sanitize_filename(filename):
+    return re.sub(r"[^\w]", "", filename)
+
+
+# Function to generate images of text
+def generate_text_images(sequence, font_path, output_folder):
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Set the desired image size and font size
+    image_size = (450, 40)
+    font_size = 17
+
+    # Load the TrueType font
+    font = ImageFont.truetype(font_path, font_size)
+
+    # Create a new blank image with a white background
+    image = Image.new("RGB", image_size, "white")
+    draw = ImageDraw.Draw(image)
+
+    # Calculate the text bounding box to center it in the image
+    text_bbox = draw.textbbox((0, 0, image_size[0], image_size[1]), sequence, font=font)
+    text_width = text_bbox[2] - text_bbox[0]
+    text_height = text_bbox[3] - text_bbox[1]
+    x = (image_size[0] - text_width) // 2
+    y = (image_size[1] - text_height) // 2
+
+    # Draw the text on the image
+    draw.text((x, y), sequence, font=font, fill="black")
+
+    # Sanitize the filename
+    sanitized_sequence = sanitize_filename(sequence)
+
+    # Save the image as a PNG file
+    output_path = os.path.join(output_folder, f"{sanitized_sequence}.png")
+    image.save(output_path)
+    # Write to file sequence and filename
+    with open("samples/index.txt", "a") as f:
+        f.write(f"{sequence} {output_path}\n")
+    print(f"Saved image: {output_path}"
+          f" ({remaining_samples}/{sample_count}) - {estimated_time:.2f}s left", end="\r")
+
+
+# Recalculate estimated time, based on remaining samples and average download time
+def recalculate_estimated_time(remaining_samples):
+    global estimated_time
+    estimated_time = remaining_samples * 0.5
+
+
+def generateSequence(charset, length):
+    sequence = ""
+    for i in range(length):
+        sequence += random.choice(charset)
+    return sequence
+
+
+sample_count = 1000
+offset = 0
+sample_output = "./samples"
+font_path = "./unispace.ttf"
+remaining_samples = sample_count
+estimated_time = remaining_samples * 0.5
+original_estimated_time = estimated_time
+charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&*?!+-"
+sequence_length = 40
+
+# Download samples
+for i in range(offset, sample_count):
+    recalculate_estimated_time(remaining_samples)
+    generate_text_images(generateSequence(charset, sequence_length), font_path, sample_output)
+    remaining_samples -= 1
---
+++ captcha1/train.py
@@ -0,0 +1,126 @@
+import stow
+import tensorflow
+from keras import Model
+from keras.applications.densenet import layers
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
+from mltu.callbacks import TrainLogger, Model2onnx
+from mltu.losses import CTCloss
+from mltu.metrics import CWERMetric
+from mltu.model_utils import residual_block
+
+from configs import ModelConfigs
+from mltu.dataProvider import DataProvider
+from mltu.preprocessors import ImageReader
+from mltu.transformers import ImageResizer, LabelIndexer, LabelPadding
+from mltu.augmentors import RandomBrightness, RandomRotate, RandomErodeDilate
+
+
+def train_model(input_dim, output_dim, activation='leaky_relu', dropout=0.2):
+    print("training model")
+    inputs = layers.Input(shape=input_dim, name="input")
+
+    # normalize images here instead in preprocessing step
+    input = layers.Lambda(lambda x: x / 255)(inputs)
+
+    x1 = residual_block(input, 16, activation=activation, skip_conv=True, strides=1, dropout=dropout)
+
+    x2 = residual_block(x1, 16, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x3 = residual_block(x2, 16, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+
+    x4 = residual_block(x3, 32, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x5 = residual_block(x4, 32, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+
+    x6 = residual_block(x5, 64, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x7 = residual_block(x6, 32, activation=activation, skip_conv=True, strides=1, dropout=dropout)
+
+    x8 = residual_block(x7, 64, activation=activation, skip_conv=True, strides=2, dropout=dropout)
+    x9 = residual_block(x8, 64, activation=activation, skip_conv=False, strides=1, dropout=dropout)
+
+    squeezed = layers.Reshape((x9.shape[-3] * x9.shape[-2], x9.shape[-1]))(x9)
+
+    blstm = layers.Bidirectional(layers.LSTM(128, return_sequences=True))(squeezed)
+    blstm = layers.Dropout(dropout)(blstm)
+
+    output = layers.Dense(output_dim + 1, activation='softmax', name="output")(blstm)
+
+    model = Model(inputs=inputs, outputs=output)
+    return model
+
+
+# Load dataset
+print("loading dataset")
+dataset, vocab, max_len = [], set(), 0
+for line in open("samples/index.txt"):
+    parts = line.split(" ")
+    dataset.append([stow.relpath(parts[1]), parts[0]])
+    vocab.update(list(parts[0]))
+    max_len = max(max_len, len(parts[0]))
+    print(f"loading {parts[1]}", end="\r")
+
+print("dataset loaded")
+print("Generating configs")
+configs = ModelConfigs()
+
+# Save vocab and maximum text length to configs
+configs.vocab = sorted(list(vocab))
+configs.max_text_length = max_len
+configs.save()
+print("configs saved")
+
+# Define data provider
+data_provider = DataProvider(
+    dataset=dataset,
+    skip_validation=True,
+    batch_size=configs.batch_size,
+    data_preprocessors=[ImageReader()],
+    transformers=[
+        ImageResizer(configs.width, configs.height),
+        LabelIndexer(configs.vocab),
+        LabelPadding(max_word_length=configs.max_text_length, padding_value=len(configs.vocab))
+    ],
+)
+print("data provider defined")
+
+train_data_provider, val_data_provider = data_provider.split(split=0.9)
+
+train_data_provider.augmentors = [RandomBrightness(), RandomRotate(), RandomErodeDilate()]
+
+# Define model
+print("defining model")
+model = train_model(
+    input_dim=(configs.height, configs.width, 3),
+    output_dim=len(configs.vocab),
+)
+
+# Compile model
+print("compiling model")
+model.compile(
+    optimizer=tensorflow.keras.optimizers.Adam(learning_rate=configs.learning_rate),
+    loss=CTCloss(),
+    metrics=[CWERMetric()],
+)
+
+# Define callbacks
+earlystopper = EarlyStopping(monitor='val_CER', patience=40, verbose=1)
+checkpoint = ModelCheckpoint(f"{configs.model_path}/model.h5", monitor='val_CER', verbose=1, save_best_only=True,
+                             mode='min')
+trainLogger = TrainLogger(configs.model_path)
+tb_callback = TensorBoard(f'{configs.model_path}/logs', update_freq=1)
+reduceLROnPlat = ReduceLROnPlateau(monitor='val_CER', factor=0.9, min_delta=1e-10, patience=20, verbose=1, mode='auto')
+model2onnx = Model2onnx(f"{configs.model_path}/model.h5")
+
+# Train the model
+print("Starting to train model")
+model.fit(
+    train_data_provider,
+    validation_data=val_data_provider,
+    epochs=configs.train_epochs,
+    callbacks=[earlystopper, checkpoint, trainLogger, reduceLROnPlat, tb_callback, model2onnx],
+    workers=configs.train_workers
+)
+print("model trained")
+
+print("saving model")
+# Save training and validation datasets as csv files
+train_data_provider.to_csv(stow.join(configs.model_path, 'train.csv'))
+val_data_provider.to_csv(stow.join(configs.model_path, 'val.csv'))
