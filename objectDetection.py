75d7917f89e06908f0640e8f8b8153e4aebc3b4f
TAATEEL1 <elio.teuber@swisscom.com>
2023-11-03 23:12:19 +0100

    feat: adding some new things to the project

--- objectDetection.py
+++ objectDetection.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def getContours(img_path='image.jpg'):
+def getContours(img_path="image.jpg"):
     # Define the lower and upper bounds for the color of emojis (adjust these as needed)
     lower_color = np.array(0)
     upper_color = np.array(200)
@@ -14,13 +14,15 @@ def getContours(img_path='image.jpg'):
     color_mask = cv2.inRange(img, lower_color, upper_color)
 
     # Find contours in the mask
-    contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(
+        color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
 
     _, img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
 
     # Define a minimum and maximum size for emojis (adjust these as needed)
-    min_size = 80
-    max_size = 1000
+    min_size = 5000
+    max_size = 50000
 
     detected_emojis = []
 
@@ -37,22 +39,22 @@ def getContours(img_path='image.jpg'):
         # Filter contours based on size
         if min_size < area < max_size:
             x, y, w, h = cv2.boundingRect(contour)
-            img_rect = cv2.resize(img[y - 2:y + h + 2, x - 2:x + w + 2], (30, 30))
+            img_rect = cv2.resize(img[y - 2 : y + h + 2, x - 2 : x + w + 2], (30, 30))
             detected_emojis.append(img_rect)
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Save or display the image with rectangles
-    if __name__ == '__main__':
-        cv2.imwrite('output_image.png', img)
-        cv2.imshow('Image with Rectangles', img)
+    if __name__ == "__main__":
+        cv2.imwrite("output_image.png", img)
+        cv2.imshow("Image with Rectangles", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     return detected_emojis, contours
 
 
-if __name__ == '__main__':
-    detected_emojis, _ = getContours('image.jpg')
+if __name__ == "__main__":
+    detected_emojis, _ = getContours("image.jpg")
     # Save to emojis folder enumerated
     for i, emoji in enumerate(detected_emojis):
-        cv2.imwrite('emojis/' + str(i) + '.png', emoji)
+        cv2.imwrite("emojis/" + str(i) + ".png", emoji)
--- sample_collector.py
+++ sample_collector.py
@@ -22,17 +22,21 @@ def generate_image(char, rotate=True, **kwargs):
     # os.makedirs(output_folder, exist_ok=True)
 
     # Set the desired image size and font size
-    if 'image_size' in kwargs:
-        image_size = kwargs['image_size']
+    if "image_size" in kwargs:
+        image_size = kwargs["image_size"]
     else:
         image_size = (configs.char_width, configs.char_height)
 
-    char_img = Image.open(char['file'])
+    char_img = Image.open(char["file"])
     new_img = char_img.resize(image_size)
 
     if rotate:
         rotation_fraction = (360 / 8) + random.randint(-5, 5)
-        new_img = new_img.rotate(rotation_fraction * random.randint(0, 8), fillcolor=255, resample=Image.BILINEAR)
+        new_img = new_img.rotate(
+            rotation_fraction * random.randint(0, 8),
+            fillcolor=255,
+            resample=Image.BILINEAR,
+        )
 
     new_img = new_img.convert("RGBA")
     # Blur the image
@@ -64,14 +68,27 @@ def generate_image_sequence(output_folder, file_nr):
     char_width = 26
     char_height = char_width
 
-    img = Image.new('RGB', (450, 60), color=(255, 255, 255))
+    img = Image.new("RGB", (450, 60), color=(255, 255, 255))
     for i, char in enumerate(sequence):
-        char_img = generate_image(char, image_size=(int(char_width), int(char_height)), rotate=True)
+        char_img = generate_image(
+            char, image_size=(int(char_width), int(char_height)), rotate=True
+        )
         vertival_offset = random.randint(-12, 12)
         horizontal_offset = random.randint(-10, 10)
         # img.paste(char_img, (int(i * char_width + (i + 1) * char_spacing), int(char_height / 2)))
-        img.paste(char_img, (int(i * char_width + i * char_spacing + horizontal_offset + (char_width / 2)),
-                             int(char_height / 2 + vertival_offset)), char_img)
+        img.paste(
+            char_img,
+            (
+                int(
+                    i * char_width
+                    + i * char_spacing
+                    + horizontal_offset
+                    + (char_width / 2)
+                ),
+                int(char_height / 2 + vertival_offset),
+            ),
+            char_img,
+        )
 
     img = img.resize((configs.width, configs.height), Image.LANCZOS)
     img.save(os.path.join(output_folder, f"{str(file_nr)}.png"))
@@ -79,14 +96,17 @@ def generate_image_sequence(output_folder, file_nr):
     # Save the image as a PNG file
     output_path = f"{str(file_nr)}.png"
 
-    char_sequence = "".join([char['char'] for char in sequence])
+    char_sequence = "".join([char["char"] for char in sequence])
 
     # Write to file sequence and filename
     with open(os.path.join(output_folder, "index.txt"), "a") as f:
         f.write(f"{char_sequence} {output_path}\n")
 
-    print(f"Saved image: {output_path}"
-          f" ({remaining_samples}/{sample_count}) - {estimated_time:.2f}s left", end="\r")
+    print(
+        f"Saved image: {output_path}"
+        f" ({sample_count - remaining_samples}/{sample_count}) - {estimated_time:.2f}s left",
+        end="\r",
+    )
 
 
 # Recalculate estimated time, based on remaining samples and average download time
@@ -111,21 +131,21 @@ estimated_time = remaining_samples * 0.5
 original_estimated_time = estimated_time
 
 charMap = [
-    {'symbol': ':D', 'char': 'A', 'file': 'emojis/0.png'},
-    {'symbol': ':)', 'char': 'B', 'file': 'emojis/1.png'},
-    {'symbol': ':P', 'char': 'C', 'file': 'emojis/2.png'},
-    {'symbol': ':(', 'char': 'D', 'file': 'emojis/3.png'},
-    {'symbol': ';)', 'char': 'E', 'file': 'emojis/4.png'},
-    {'symbol': 'B)', 'char': 'F', 'file': 'emojis/5.png'},
-    {'symbol': ':@', 'char': 'G', 'file': 'emojis/6.png'},
-    {'symbol': ':o', 'char': 'H', 'file': 'emojis/7.png'},
-    {'symbol': ':S', 'char': 'I', 'file': 'emojis/8.png'},
-    {'symbol': ':|', 'char': 'J', 'file': 'emojis/9.png'},
-    {'symbol': ':/', 'char': 'K', 'file': 'emojis/10.png'},
-    {'symbol': '<3', 'char': 'L', 'file': 'emojis/11.png'}
+    {"symbol": ":D", "char": "A", "file": "emojis/0.png"},
+    {"symbol": ":)", "char": "B", "file": "emojis/1.png"},
+    {"symbol": ":P", "char": "C", "file": "emojis/2.png"},
+    {"symbol": ":(", "char": "D", "file": "emojis/3.png"},
+    {"symbol": ";)", "char": "E", "file": "emojis/4.png"},
+    {"symbol": "B)", "char": "F", "file": "emojis/5.png"},
+    {"symbol": ":@", "char": "G", "file": "emojis/6.png"},
+    {"symbol": ":o", "char": "H", "file": "emojis/7.png"},
+    {"symbol": ":S", "char": "I", "file": "emojis/8.png"},
+    {"symbol": ":|", "char": "J", "file": "emojis/9.png"},
+    {"symbol": ":/", "char": "K", "file": "emojis/10.png"},
+    {"symbol": "<3", "char": "L", "file": "emojis/11.png"},
 ]
 # I only want the char
-charset = [char['char'] for char in charMap]
+charset = [char["char"] for char in charMap]
 sequence_length = configs.max_text_length
 
 generate_image_sequence(sample_output, 0)
